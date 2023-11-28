c1a0796bae8b3993e7484abc62f85503fc4bfb49
cesarmi7 <cesarandres2010@hotmail.com>
2023-11-28 02:06:19 -0500

    hola!

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,11 +1,24 @@
-import Link from "next/link"
+import Link from "next/link";
 
-const NavLink = ({href, title}) => {
-    return(
-        <Link href={href} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
-            {title}
+const NavLink = ({ href, title }) => {
+    const handleClick = (e) => {
+        // Check if it's a hash link
+        if (href.startsWith("#")) {
+            e.preventDefault(); // Prevent default anchor behavior
+            const element = document.querySelector(href); // Find the element
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' }); // Smooth scroll to the element
+            }
+        }
+    };
+
+    return (
+        <Link href={href} passHref>
+            <a onClick={handleClick} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
+                {title}
+            </a>
         </Link>
-    )
-}
+    );
+};
 
-export default NavLink
+export default NavLink;
