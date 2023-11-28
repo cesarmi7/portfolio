52f58eaf1339ff5c2095d6eed6f827c379cd2ad6
cesarmi7 <cesarandres2010@hotmail.com>
2023-11-28 15:55:44 -0500

    hola!

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -43,8 +43,10 @@ const HeroSection = () => {
           onClick={() => document.getElementById('emailSection').scrollIntoView({ behavior: 'smooth' })}>
             Contact Me
           </button>
-          <a href="../../CesarResume.pdf" download className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-600 via-purple-600 to-red-600 hover:bg-slate-800 text-white mt-3'>
+          <a href="/CesarResume.pdf" download>
+          <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-600 via-purple-600 to-red-600 hover:bg-slate-800 text-white mt-3'>
             <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+          </button>
           </a>
         </div>
       </motion.div>
