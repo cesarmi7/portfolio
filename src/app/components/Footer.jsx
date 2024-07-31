9918490eabf20d881a38f869bcc68aa3d8bf3265
cesarmi7 <cesarandres2010@hotmail.com>
2024-07-30 22:55:38 -0400

    hola

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 
 const Footer = () => {
@@ -12,3 +13,19 @@ const Footer = () => {
 }
 
 export default Footer
+=======
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footer border z-10 border-top-[#33353F] border-l-transparent border-r-transparent text-white'>
+        <div className='container p-12 flex justify-between'>
+            <span>CSAR</span>
+            <p className='text-slate-600'>All rights reserved.</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
+>>>>>>> 139cfd5 (hola)
